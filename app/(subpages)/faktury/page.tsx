69f59967d752c7d62fcfcfc599749a646ead0337
davidnx01 <info@webdavid.sk>
2025-10-27 14:37:59 +0100

    added: faktury page

---
+++ app/(subpages)/faktury/page.tsx
@@ -0,0 +1,38 @@
+import { PageTabs } from "@/components/ui/page-tabs";
+import { SubpageHeading } from "@/components/ui/subpage-heading";
+import { fetchData, fetchGeneral } from "@/lib/api";
+import { TPage } from "@/types/page";
+
+import { generateMetadata as generateSharedMetadata } from "@/hooks/generate-metadata";
+import type { TGeneral } from "@/types/general";
+
+export async function generateMetadata() {
+  const [page, general] = await Promise.all([
+    fetchData("faktury-page", {
+      populate: ["seo", "seo.open_graph"],
+    }) as Promise<TPage>,
+    fetchGeneral() as Promise<TGeneral>,
+  ]);
+
+  return generateSharedMetadata({
+    seo: page.seo,
+    general,
+  });
+}
+
+export default async function Page() {
+  const page = (await fetchData("faktury-page", {
+    populate: ["heading", "heading.image", "tabs", "tabs.files"],
+  })) as TPage;
+
+  return (
+    <>
+      <SubpageHeading
+        image={page?.heading?.image}
+        title={page?.heading?.title}
+        description={page?.heading?.description}
+      />
+      <PageTabs tabs={page.tabs} />
+    </>
+  );
+}
