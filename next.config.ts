f39dbe09628361d2d3880c40d1a43d6f2a50d56c
davidnx01 <info@webdavid.sk>
2025-11-06 15:06:05 +0100

    added: domains

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['localhost', 'domovrosa-api.tyvole.sk'],
+    domains: ['localhost', 'domovrosa-api.tyvole.sk', 'domovrosa.sk', 'domovrosa-api.sk'],
   },
   typescript: {
     ignoreBuildErrors: true,
